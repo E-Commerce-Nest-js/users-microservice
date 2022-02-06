acb71d49415ebbaee46a7aa9bae5681ad52e2758
Igor Tkachenko <stiles.2126@gmail.com>
2022-02-06 10:05:23 +1000

    feat: add mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        TypegooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: getMongoConfig,
+        }),
+    ],
     controllers: [],
     providers: [],
 })
